3df41a0f6340f17968516f13aef31afe87636b28
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-06-16 00:18:24 +0200

    Update YourGroupFragment.java

--- app/src/main/java/com/example/projektwtm/YourGroupFragment.java
+++ app/src/main/java/com/example/projektwtm/YourGroupFragment.java
@@ -2,63 +2,65 @@ package com.example.projektwtm;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.regex.Pattern;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link YourGroupFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class YourGroupFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public YourGroupFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment YourGroupFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static YourGroupFragment newInstance(String param1, String param2) {
-        YourGroupFragment fragment = new YourGroupFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_your_group, container, false);
     }
+
+
+//    EditText paymentDeadlineET = findViewById(R.id.editText18);
+//    EditText accountNumberET = findViewById(R.id.editText19);
+//    EditText loginET = findViewById(R.id.editText20);
+//    EditText pass1ET = findViewById(R.id.editText21);
+//    EditText pass2ET = findViewById(R.id.editText22);
+//
+//    final String paymentDeadline = paymentDeadlineET.getText().toString();
+//    final String accountNumber = accountNumberET.getText().toString();
+//    final String login = loginET.getText().toString();
+//    final String pass1 = pass1ET.getText().toString();
+//    final String pass2 = pass2ET.getText().toString();
+
+//    Button button = findViewById(R.id.button13);
+//        button.setOnClickListener(new View.OnClickListener(){
+//        public void onClick(View v){
+//                boolean change = changeGroup(paymentDeadline, accountNumber, login, pass1, pass2);
+//                if (log) {
+//modyfikacja danych
+//                    TextView error = findViewById(R.id.textView50);
+//                    error.setText("");
+//podmiana fragmentu na ten sam
+//                }
+//                else {
+//                    TextView error = findViewById(R.id.textView50);
+//                    error.setText("Incorrect data. Try again.");
+//podmiana fragmentu na ten sam
+//                }
+//        }
+//    });
+
+
+//    public boolean changeGroup(String paymentDeadline, String accountNumber, String login, String pass1, String pass2) {
+//        if (paymentDeadline.equals("") || accountNumber.equals("") || login.equals("") || pass1.equals("") || pass2.equals("")) {
+//            return false;
+//        }
+//        else if (!pass1.equals(pass2)) {
+//            return false;
+//        }
+//        else if (!Pattern.matches("20[2-9]{1}[0-9]{1}-[1-9]{1,2}-[1-3]{0,1}[0-9]", paymentDeadline) || !Pattern.matches("[0-9]+", accountNumber) || !Pattern.matches("[a-zA-Z0-9]", login) || !Pattern.matches("[A-Za-z1-9_!@]+", pass1)  || !Pattern.matches("[A-Za-z1-9_!@]+", pass2)) {
+//            return false;
+//        }
+//
+//        return true;
+//    }
 }

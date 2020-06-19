bf48bec1872ff0b407bee40eab8a4697669d9f88
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-06-20 00:04:26 +0200

    Update AddGroupFragment.java

--- apka/app/src/main/java/com/example/projektwtm/AddGroupFragment.java
+++ apka/app/src/main/java/com/example/projektwtm/AddGroupFragment.java
@@ -10,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.regex.Pattern;
 
@@ -17,6 +18,35 @@ public class AddGroupFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        EditText paymentDeadlineET = getView().findViewById(R.id.editText);
+        EditText accountNumberET = getView().findViewById(R.id.editText8);
+        EditText loginET = getView().findViewById(R.id.editText13);
+        EditText pass1ET = getView().findViewById(R.id.editText14);
+        EditText pass2ET = getView().findViewById(R.id.editText15);
+
+        final String paymentDeadline = paymentDeadlineET.getText().toString();
+        final String accountNumber = accountNumberET.getText().toString();
+        final String login = loginET.getText().toString();
+        final String pass1 = pass1ET.getText().toString();
+        final String pass2 = pass2ET.getText().toString();
+
+        Button button = getView().findViewById(R.id.button13);
+            button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                boolean add = addGroup(paymentDeadline, accountNumber, login, pass1, pass2);
+                if (add) {
+                     //dodanie grupy do bazy danych wraz z przypisaniem jej wartosci domyslnych aplikacji i pakietu
+                    TextView error = getView().findViewById(R.id.textView62);
+                    error.setText("");
+                    //podmiana fragmentu na widok szczegolowy grupy, ktorej sie jest wlascicielem
+                }
+                else {
+                    TextView error = getView().findViewById(R.id.textView62);
+                    error.setText("Incorrect data. Try again.");
+                }
+        }
+    });
+
         return inflater.inflate(R.layout.fragment_add_group, container, false);
     }
 
@@ -31,39 +61,9 @@ public class AddGroupFragment extends Fragment {
             return false;
         }
 
-        //sprawdzenie w bazie danych
+        //sprawdzenie w bazie danych, czy dany uzytkownik nie ma juz konta na dany login konta
 
         return true;
     }
-    
-//    EditText paymentDeadlineET = findViewById(R.id.editText);
-//    EditText accountNumberET = findViewById(R.id.editText8);
-//    EditText loginET = findViewById(R.id.editText13);
-//    EditText pass1ET = findViewById(R.id.editText14);
-//    EditText pass2ET = findViewById(R.id.editText15);
-//
-//    final String paymentDeadline = paymentDeadlineET.getText().toString();
-//    final String accountNumber = accountNumberET.getText().toString();
-//    final String login = loginET.getText().toString();
-//    final String pass1 = pass1ET.getText().toString();
-//    final String pass2 = pass2ET.getText().toString();
-    
-//    Button button = findViewById(R.id.button13);
-//        button.setOnClickListener(new View.OnClickListener(){
-//        public void onClick(View v){
-//                boolean add = addGroup(paymentDeadline, accountNumber, login, pass1, pass2);
-//                if (log) {
-// dodanie nowej grupy do bazy danych
-//                    TextView error = findViewById(R.id.textView62);
-//                    error.setText("");
-//podmiana fragmentu
-//                }
-//                else {
-//                    TextView error = findViewById(R.id.textView62);
-//                    error.setText("Incorrect data. Try again.");
-//podmiana fragmentu na ten sam
-//                }
-//        }
-//    });    
-    
+
 }

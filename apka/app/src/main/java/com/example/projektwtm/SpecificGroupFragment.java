c5a613584272f3813582795b9d4f76f06d6659b7
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-06-20 00:08:51 +0200

    Update SpecificGroupFragment.java

--- apka/app/src/main/java/com/example/projektwtm/SpecificGroupFragment.java
+++ apka/app/src/main/java/com/example/projektwtm/SpecificGroupFragment.java
@@ -8,20 +8,23 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SpecificGroupFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            Button button = getView().findViewById(R.id.button8);
+            button.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                //dodanie uzytkownika do grupy
+                //podmiana fragmentu na widok grupy, do ktorej sie teraz nalezy, czyli ten bardziej szczegolowy widok
+            }
+             });
+
         return inflater.inflate(R.layout.fragment_specific_group, container, false);
     }
 
-//    Button button = findViewById(R.id.button8);
-//        button.setOnClickListener(new View.OnClickListener(){
-//        public void onClick(View v){
-//dodanie grupy uzytkownikowi
-//podmiana fragmentu na widok grupy, do ktorej sie teraz nalezy
-//    });
-//    }
-    
+
+
 }

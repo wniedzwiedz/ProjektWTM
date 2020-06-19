00b660e9603a7e16a4462dbce075fa59d3d6d32a
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-06-20 00:05:29 +0200

    Update MainActivity.java

--- apka/app/src/main/java/com/example/projektwtm/MainActivity.java
+++ apka/app/src/main/java/com/example/projektwtm/MainActivity.java
@@ -4,14 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PatternMatcher;
 import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.projektwtm.modele.User;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.regex.Pattern;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,37 +56,60 @@ public class MainActivity extends AppCompatActivity {
         Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-//odkomentowac, gdy bedzie juz mozliwosc rejestracji
-//                boolean log = register(firstname, surname, email, pass1, pass2);
-//                if (log) {
-// dodanie nowego uzytkownika do bazy danych
-//                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//                    TextView error = findViewById(R.id.textView17);
-//                    error.setText("");
-//                    startActivity(intent);
-//                }
-//                else {
-//                    Intent intent = new Intent(MainActivity.this, MainActivity.class);
-//                    TextView error = findViewById(R.id.textView17);
-//                    error.setText("Incorrect data. Try again.");
-//                    startActivity(intent);
-//                }
+                boolean reg = false;
+                try {
+                    reg = register(firstname, surname, email, pass1, pass2);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                if (reg) {
+                   User user = new User(firstname, surname, email, pass1);
+
+                    ResteasyClient client = new ResteasyClientBuilder().build();
+                    ResteasyWebTarget target = client.target("http://localhost:8080/FindCo/api/users/");
+                    Response response = target.request().post(Entity.entity(user, MediaType.APPLICATION_JSON));
+                    response.close();
+
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    TextView error = findViewById(R.id.textView17);
+                    error.setText("");
+                    startActivity(intent);
+                }
+                else {
+                    //Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                    TextView error = findViewById(R.id.textView17);
+                    error.setText("Incorrect data. Try again.");
+                    //startActivity(intent);
+                }
             }
         });
     }
 
-    public boolean register(String firstname, String surname, String email, String pass1, String pass2) {
+    public boolean register(String firstname, String surname, String email, String pass1, String pass2) throws JSONException {
         if (firstname.equals("") || surname.equals("") || email.equals("") || pass1.equals("") || pass2.equals("")) {
             return false;
         }
-        else if (!Pattern.matches("[A-Za-z]+", firstname) || !Pattern.matches("[A-Za-z]+", surname) || !Pattern.matches("[A-Za-z]+[1-9_]*[@]{1}[A-Za-z1-9]+[.]{1}[a-z]+[.]{0,1}[a-z]+}", email) || !Pattern.matches("[A-Za-z1-9_!@]+", pass1)  || !Pattern.matches("[A-Za-z1-9_!@]+", pass2)) {
+        else if (!Pattern.matches("[A-Za-z]+", firstname) || !Pattern.matches("[A-Za-z]+", surname) /*|| !Pattern.matches("[A-Za-z]+[1-9_]*[@]{1}[A-Za-z1-9]+[.]{1}[a-z]+[.]{0,1}[a-z]+}", email) || !Pattern.matches("[A-Za-z1-9_!@]+", pass1)  || !Pattern.matches("[A-Za-z1-9_!@]+", pass2) */) {
             return false;
         }
         else if (!pass1.equals(pass2)) {
             return false;
         }
 
-        //sprawdzenie w bazie danych
+        ResteasyClient client = new ResteasyClientBuilder().build();
+        ResteasyWebTarget target = client.target("http://localhost:8080/FindCo/api/users/");
+        Response response = target.request().get();
+        if (response.hasEntity()) {
+            JSONArray users = (JSONArray) response.getEntity();
+            for (int i = 0; i < users.length(); ++i) {
+                JSONObject obj = users.getJSONObject(i);
+                String emailJSON = obj.getString("email");
+                if (email.equals(emailJSON)) {
+                    return false;
+                }
+            }
+        }
+        response.close();
 
         return true;
     }

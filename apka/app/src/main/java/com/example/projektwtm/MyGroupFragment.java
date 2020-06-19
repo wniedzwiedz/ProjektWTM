6427e894f766a353e167a70c597ba441eb9032aa
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-06-20 00:06:34 +0200

    Update MyGroupFragment.java

--- apka/app/src/main/java/com/example/projektwtm/MyGroupFragment.java
+++ apka/app/src/main/java/com/example/projektwtm/MyGroupFragment.java
@@ -8,18 +8,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MyGroupFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //wyswietlenie odpowiednich danych o grupie
+
+       Button button = getView().findViewById(R.id.button5);
+       button.setOnClickListener(new View.OnClickListener(){
+        public void onClick(View v) {
+            //usuniecie uzytkownika z grupy
+            //podmiana fragmentu na widok grup uzytkownika
+        }
+        });
+
         return inflater.inflate(R.layout.fragment_my_group, container, false);
     }
-    
-//    Button button = findViewById(R.id.button5);
-//        button.setOnClickListener(new View.OnClickListener(){
-//        public void onClick(View v){
-//usuniecie grupy z grup uzytkownika, do ktorych nalezy
-//podmiana fragmentu na inny
-//    });
 }

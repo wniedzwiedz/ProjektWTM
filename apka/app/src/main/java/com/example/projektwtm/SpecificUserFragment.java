db8c7f5cf498a2c3d5b9a1ccc26bb23a17dc58ad
kkoziol72 <33664563+kkoziol72@users.noreply.github.com>
2020-06-21 01:41:58 +0200

    Update SpecificUserFragment.java

--- apka/app/src/main/java/com/example/projektwtm/SpecificUserFragment.java
+++ apka/app/src/main/java/com/example/projektwtm/SpecificUserFragment.java
@@ -8,12 +8,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SpecificUserFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //podmiana fragmentu na widok konta danego uzytkownika
+        TextView login = getView().findViewById(R.id.textView33);
+        TextView firstname = getView().findViewById(R.id.textView36);
+        TextView surname = getView().findViewById(R.id.textView38);
+        LinearLayout linearLayout = getView().findViewById(R.id.linearLayout1);
+
+        //przypisanie danych do text view w widoku uzytkownika
+        //dodanie do linear layout grup użytkownika
+        
         return inflater.inflate(R.layout.fragment_specific_user, container, false);
     }
 }
